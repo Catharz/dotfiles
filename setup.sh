9ce7f961cc0e1b5c15802b5480f271897cb4f857
Craig Read <craig.read@redbubble.com>
2017-08-17 16:45:01 +1000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+brew eundle
+
+ln -s ./ruby/bundle ${HOME}/.bundle
+ln -s ./ruby/gemrc ${HOME}/.gemrc
+ln -s ./ruby/pryrc ${HOME}/.pryrc
+
+ln -s ./emacs/emacs.d ${HOME}/.emacs.d
+ln -s ./emacs/spacemacs ${HOME}/.spacemacs
+
+ln -s ./git/gitconfig ${HOME}/.gitconfig
+
+ln -s ./zsh/zhsenv ${HOME}/.zshenv
+ln -s ./zsh/zhsrc ${HOME}/.zshrc
